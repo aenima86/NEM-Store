7679ecf6df79ac20dd10348acce81525295593da
aenima86 <appsileci@gmail.com>
2018-06-18 12:05:59 +0200

    Update storedoc.js

--- store template/storedoc.js
+++ store template/storedoc.js
@@ -7,18 +7,21 @@
   "products": {
     "item0": {
       "id": "0",
+      "title": "Item 0",
       "desc": "Test item 0",
       "img": "i.imgur.com/ZGTOl8Y.jpg",
       "price": "10"
     },
     "item1": {
       "id": "1",
+      "title": "Item 1",
       "desc": "Test item 1",
       "img": "i.imgur.com/hkequMC.jpg",
       "price": "20"
     },
     "item2": {
       "id": "2",
+      "title": "Item 2",
       "desc": "Test item 2",
       "img": "i.imgur.com/pkwbpVr.jpg",
       "price": "30"
