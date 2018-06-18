b3058f17ce30821f60617dbac13074e7513a2ffe
aenima86 <appsileci@gmail.com>
2018-06-18 09:59:38 +0200

    Update storedoc.js

--- store template/storedoc.js
+++ store template/storedoc.js
@@ -1,5 +1,27 @@
-{ 
-  "firstName": "John",
-  "lastName": "Doe",
-  "age": 25
+{
+  "type": "nemstore",
+  "title": "MyStore",
+  "namespace": "myNameSpace",
+  "desc": "my nem store describtion",
+  "shipping": "US,EU,CN",
+  "products": {
+    "item0": {
+      "id": "0",
+      "desc": "Test item 0",
+      "img": "i.imgur.com/ZGTOl8Y.jpg",
+      "price": "10"
+    },
+    "item1": {
+      "id": "1",
+      "desc": "Test item 1",
+      "img": "i.imgur.com/hkequMC.jpg",
+      "price": "20"
+    },
+    "item2": {
+      "id": "2",
+      "desc": "Test item 2",
+      "img": "i.imgur.com/pkwbpVr.jpg",
+      "price": "30"
+    }
+  }
 }
