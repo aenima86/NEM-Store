74909e0f855ee0ed91e52acd9a4275cc69f1f16e
aenima86 <appsileci@gmail.com>
2018-06-20 12:21:50 +0200

    Update storedoc.js

--- store template/storedoc.js
+++ store template/storedoc.js
@@ -2,27 +2,27 @@
   "type": "nemstore",
   "title": "MyStore",
   "namespace": "myNameSpace",
-  "desc": "my nem store describtion",
+  "desc": "My nem store describtion...Lorem dui cubilia vivamus torquent placerat lectus eu magna, potenti torquent ridiculus auctor est dictumst, eros hac nisl pretium elementum porta conubia litora netus. In rhoncus porttitor nec per Tempor nunc habitant. ",
   "shipping": "US,EU,CN",
   "products": {
     "item0": {
       "id": "0",
       "title": "Item 0",
-      "desc": "Test item 0",
+      "desc": "Egestas. Nascetur proin curae; per posuere hac aptent magnis inceptos malesuada fermentum consectetuer cubilia platea vitae fermentum. Maecenas ante consectetuer ridiculus lacus curae; posuere maecenas.",
       "img": "i.imgur.com/ZGTOl8Y.jpg",
       "price": "10"
     },
     "item1": {
       "id": "1",
       "title": "Item 1",
-      "desc": "Test item 1",
+      "desc": "Cum. Justo cubilia malesuada adipiscing duis. Fusce eleifend adipiscing velit est purus pede posuere eros facilisi varius egestas pede urna feugiat torquent odio bibendum habitasse. Lacus Velit iaculis pulvinar venenatis donec taciti senectus.",
       "img": "i.imgur.com/hkequMC.jpg",
       "price": "20"
     },
     "item2": {
       "id": "2",
       "title": "Item 2",
-      "desc": "Test item 2",
+      "desc": "Velit nunc neque laoreet pharetra. Iaculis mauris pharetra fringilla sed nullam, fermentum congue mollis mi eget phasellus litora euismod nullam at. Felis quis sem lacinia, mi pharetra phasellus litora nulla ipsum vivamus praesent vulputate. Blandit hac magnis suspendisse, nulla. Eros nulla varius.",
       "img": "i.imgur.com/pkwbpVr.jpg",
       "price": "30"
     }
